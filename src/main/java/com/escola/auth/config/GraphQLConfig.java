6f02171d757837ff4a96618a665e430479bc14d5
Rogério Cardoso <rogerio.cardoso@trt18.jus.br>
2025-07-13 09:47:30 -0300

    não utilizar federaão

--- src/main/java/com/escola/auth/config/GraphQLConfig.java
+++ src/main/java/com/escola/auth/config/GraphQLConfig.java
@@ -1,7 +1,6 @@
 package com.escola.auth.config;
 
 import graphql.scalars.ExtendedScalars;
-import graphql.schema.GraphQLScalarType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
@@ -15,27 +14,27 @@ public class GraphQLConfig {
      *
      * @return um configurador que adiciona os scalars necessários.
      */
-//    @Bean
-//    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
-//        return wiringBuilder -> wiringBuilder
-//                // Para o tipo java.time.Instant, o mais apropriado é o DateTime.
-//                .scalar(ExtendedScalars.DateTime)
-//                // Para o tipo java.time.LocalDate (se você usar em outro lugar).
-//                .scalar(ExtendedScalars.Date)
-//                // Para o tipo Long.
-//                .scalar(ExtendedScalars.GraphQLLong);
-//    }
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
-        // Cria um novo scalar chamado "OffsetDateTime" que usa a lógica do "DateTime"
-        GraphQLScalarType offsetDateTimeScalar = ExtendedScalars.newAliasedScalar("OffsetDateTime")
-                .aliasedScalar(ExtendedScalars.DateTime)
-                .build();
-
         return wiringBuilder -> wiringBuilder
-                // Registra nosso novo scalar com o nome que o Spring espera
-                .scalar(offsetDateTimeScalar)
-                // Registra outros scalars que você possa precisar
+                // Para o tipo java.time.Instant, o mais apropriado é o DateTime.
+                .scalar(ExtendedScalars.DateTime)
+                // Para o tipo java.time.LocalDate (se você usar em outro lugar).
+                .scalar(ExtendedScalars.Date)
+                // Para o tipo Long.
                 .scalar(ExtendedScalars.GraphQLLong);
     }
+//    @Bean
+//    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+//        // Cria um novo scalar chamado "OffsetDateTime" que usa a lógica do "DateTime"
+//        GraphQLScalarType offsetDateTimeScalar = ExtendedScalars.newAliasedScalar("OffsetDateTime")
+//                .aliasedScalar(ExtendedScalars.DateTime)
+//                .build();
+//
+//        return wiringBuilder -> wiringBuilder
+//                // Registra nosso novo scalar com o nome que o Spring espera
+//                .scalar(offsetDateTimeScalar)
+//                // Registra outros scalars que você possa precisar
+//                .scalar(ExtendedScalars.GraphQLLong);
+//    }
 }
